c3e67698a83fb0e868f4d9a4996487c9a4f862a9
objectiser <gary@pi4tech.com>
2012-08-02 18:40:59 +0100

    BAM-60 default service graph layout implementation and test

--- modules/activity-analysis/service-dependency/src/main/java/org/overlord/bam/service/dependency/layout/ServiceGraphLayoutImpl.java
+++ modules/activity-analysis/service-dependency/src/main/java/org/overlord/bam/service/dependency/layout/ServiceGraphLayoutImpl.java
@@ -17,7 +17,10 @@
  */
 package org.overlord.bam.service.dependency.layout;
 
+import org.overlord.bam.service.dependency.OperationNode;
 import org.overlord.bam.service.dependency.ServiceGraph;
+import org.overlord.bam.service.dependency.ServiceNode;
+import org.overlord.bam.service.dependency.UsageLink;
 
 /**
  * This class represents the default service graph layout
@@ -27,10 +30,131 @@ import org.overlord.bam.service.dependency.ServiceGraph;
 public class ServiceGraphLayoutImpl implements ServiceGraphLayout {
     
     /**
+     * This definition represents the width of an operation node.
+     */
+    protected static final int OPERATION_WIDTH=80;
+    
+    /**
+     * This definition represents the height of an operation node.
+     */
+    protected static final int OPERATION_HEIGHT=20;
+    
+    /**
+     * This definition represents the padding gap around the service inside.
+     */
+    protected static final int SERVICE_BORDER_PADDING=10;
+    
+    /**
+     * This definition represents the width of a service node.
+     */
+    protected static final int SERVICE_WIDTH=OPERATION_WIDTH+(2*SERVICE_BORDER_PADDING);
+    
+    /**
+     * This definition represents the padding for the service node header.
+     */
+    protected static final int SERVICE_HEADER_PADDING=20;
+    
+    /**
+     * This definition represents the horizontal padding between service nodes.
+     */
+    protected static final int SERVICE_HORIZONTAL_PADDING=100;
+    
+    /**
+     * This definition represents the vertical padding between service nodes.
+     */
+    protected static final int SERVICE_VERTICAL_PADDING=50;
+    
+    private int _height=0;
+    private int _width=0;
+    
+    /**
      * {@inheritDoc}
      */
     public void layout(ServiceGraph sg) {
         
+        // Find initial service nodes
+        for (ServiceNode sn : sg.getServiceNodes()) {
+            if (sn.getProperties().get(ServiceNode.INITIAL_NODE) == Boolean.TRUE) {
+                
+                layoutService(sg, sn, SERVICE_HORIZONTAL_PADDING,
+                            _height+SERVICE_VERTICAL_PADDING);
+            }
+        }
     }
     
+    /**
+     * This method determines whether the supplied service node
+     * requires layout.
+     * 
+     * @param sn The service node
+     * @return Whether the service node requires layout
+     */
+    protected boolean requiresLayout(ServiceNode sn) {
+        return (sn.getProperties().get(ServiceGraphLayout.X_POSITION) == null);
+    }
+    
+    /**
+     * This method performs the layout of the supplied service
+     * node.
+     * 
+     * @param sg The service graph
+     * @param sn The service node
+     * @param x The x position
+     * @param y The y position
+     */
+    protected void layoutService(ServiceGraph sg, ServiceNode sn, int x, int y) {
+        
+        // Ensure service has not already been processed
+        if (requiresLayout(sn)) {
+            
+            sn.getProperties().put(ServiceGraphLayout.X_POSITION, x);
+            sn.getProperties().put(ServiceGraphLayout.Y_POSITION, y);
+            sn.getProperties().put(ServiceGraphLayout.WIDTH, SERVICE_WIDTH);
+            sn.getProperties().put(ServiceGraphLayout.HEIGHT,
+                    SERVICE_HEADER_PADDING+((OPERATION_HEIGHT
+                            +SERVICE_BORDER_PADDING)
+                            *sn.getOperations().size()));
+            
+            int opX=x+SERVICE_BORDER_PADDING;
+            int opY=y+SERVICE_HEADER_PADDING;
+            
+            for (OperationNode opn : sn.getOperations()) {
+                opn.getProperties().put(ServiceGraphLayout.X_POSITION, opX);
+                opn.getProperties().put(ServiceGraphLayout.Y_POSITION, opY);
+                opn.getProperties().put(ServiceGraphLayout.WIDTH, OPERATION_WIDTH);
+                opn.getProperties().put(ServiceGraphLayout.HEIGHT, OPERATION_HEIGHT);
+                
+                opY += OPERATION_HEIGHT + SERVICE_BORDER_PADDING;
+            }
+            
+            // Check if width and/or height needs to be updated
+            int maxX=(Integer)sn.getProperties().get(ServiceGraphLayout.X_POSITION)
+                    +(Integer)sn.getProperties().get(ServiceGraphLayout.WIDTH);
+            
+            int maxY=(Integer)sn.getProperties().get(ServiceGraphLayout.Y_POSITION)
+                    +(Integer)sn.getProperties().get(ServiceGraphLayout.HEIGHT);
+            
+            if (maxX > _width) {
+                _width = maxX;
+            }
+            
+            if (maxY > _height) {
+                _height = maxY;
+            }
+            
+            // Identify the 'used' services to layout
+            int newX=maxX + SERVICE_HORIZONTAL_PADDING;
+            int newY=y;
+
+            for (UsageLink ul : sg.getUsageLinks()) {
+                
+                if (ul.getSource() == sn && requiresLayout(ul.getTarget())) {
+                    layoutService(sg, ul.getTarget(), newX, newY);
+                    
+                    newY += (Integer)ul.getTarget().getProperties().get(ServiceGraphLayout.HEIGHT)
+                            +SERVICE_VERTICAL_PADDING;
+                }
+            }
+        }
+    }
 }
