e824da2e1b1daf658624591d6cf805d39f34965c
objectiser <gary@pi4tech.com>
2013-03-12 10:48:38 +0000

    Support Drools event processor returning exception via handle method

--- modules/event-processor/ep-drools/src/test/java/org/overlord/rtgov/ep/drools/DroolsEventProcessorTest.java
+++ modules/event-processor/ep-drools/src/test/java/org/overlord/rtgov/ep/drools/DroolsEventProcessorTest.java
@@ -168,9 +168,60 @@ public class DroolsEventProcessorTest {
     }
     
     @Test
-    public void testPurchasingResponseTimeWithException() {
+    public void testPurchasingResponseTimeWithThrownException() {
         DroolsEventProcessor ep=new DroolsEventProcessor();
-        ep.setRuleName("PurchasingResponseTimeWithException");
+        ep.setRuleName("PurchasingResponseTimeWithThrownException");
+        
+        ActivityUnit e1=new ActivityUnit();
+        e1.setId("e1");
+        RequestReceived me1=new RequestReceived();
+        me1.setTimestamp(System.currentTimeMillis());
+        me1.setMessageId("me1");
+        e1.getActivityTypes().add(me1);
+        
+        ActivityUnit e2=new ActivityUnit();
+        e2.setId("e2");
+        ResponseSent me2=new ResponseSent();
+        me2.setMessageId("me2");
+        me2.setReplyToId("me0");
+        e2.getActivityTypes().add(me2);
+        
+        ActivityUnit e3=new ActivityUnit();
+        e3.setId("e3");
+        ResponseSent me3=new ResponseSent();
+        me3.setTimestamp(me1.getTimestamp()+TIME_INTERVAL);
+        me3.setMessageId("me3");
+        me3.setReplyToId("me1");
+        e3.getActivityTypes().add(me3);
+        
+        try {            
+            ep.init();
+            
+            java.util.Properties props1=(java.util.Properties)ep.process("Purchasing", me1, 0);
+            
+            if (props1 != null) {
+                fail("Should be no result 1");
+            }
+            
+            java.util.Properties props2=(java.util.Properties)ep.process("Purchasing", me2, 0);
+            
+            if (props2 != null) {
+                fail("Should be no result 2");
+            }
+            
+            ep.process("Purchasing", me3, 0);
+            
+            fail("Should have been interrupted by exception");
+            
+        } catch(Exception e) {
+            // Ignore
+        }
+    }
+
+    @Test
+    public void testPurchasingResponseTimeWithReturnedException() {
+        DroolsEventProcessor ep=new DroolsEventProcessor();
+        ep.setRuleName("PurchasingResponseTimeWithReturnedException");
         
         ActivityUnit e1=new ActivityUnit();
         e1.setId("e1");
