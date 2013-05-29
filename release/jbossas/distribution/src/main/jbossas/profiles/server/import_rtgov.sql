cee354e94d2807d54c733ab905dd1c2054a9f055
objectiser <gary@brownuk.com>
2013-05-29 12:03:12 +0100

    Minor changes related to using overlord-commons with gadget server

--- release/jbossas/distribution/src/main/jbossas/profiles/server/import_rtgov.sql
+++ release/jbossas/distribution/src/main/jbossas/profiles/server/import_rtgov.sql
@@ -1,5 +1,5 @@
 INSERT INTO GS_GROUP(`GROUP_ID`,`GROUP_NAME`, `GROUP_DESC`) VALUES(1, 'system', 'reserved system group');
-INSERT INTO GS_USER(`ID`, `NAME`, `DISPLAY_NAME`, `PASSWD`, `USER_ROLE`) VALUES(1, 'admin', 'Administrator', 'admin','ADMIN');
+INSERT INTO GS_USER(`ID`, `NAME`, `DISPLAY_NAME`, `USER_ROLE`) VALUES(1, 'admin', 'Administrator', 'ADMIN');
 INSERT INTO GS_USER_GROUP(`USER_ID`, `GROUP_ID`) VALUES(1, 1);
 
 INSERT INTO GS_GADGET(`GADGET_TITLE`,`GADGET_AUTHOR`,`GADGET_AUTHOR_EMAIL`,`GADGET_DESCRIPTION`,`GADGET_THUMBNAIL_URL`,`GADGET_URL`) VALUES('Response Time','Red Hat','cyu@redhat.com','Response Time Gadget','${server}/gadgets/rt-gadget/thumbnail.png','${server}/gadgets/rt-gadget/gadget.xml');
