e0b7f768139468fafed3d585f7de0f8394a2d026
objectiser <gary@brownuk.com>
2015-02-18 11:29:12 +0000

    Added info transform test for DOM to text transformation

--- modules/activity-management/activity/src/test/java/org/overlord/rtgov/activity/processor/mvel/MVELInformationTransformerTest.java
+++ modules/activity-management/activity/src/test/java/org/overlord/rtgov/activity/processor/mvel/MVELInformationTransformerTest.java
@@ -17,6 +17,8 @@ package org.overlord.rtgov.activity.processor.mvel;
 
 import static org.junit.Assert.*;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.junit.Test;
 import org.overlord.rtgov.activity.processor.mvel.MVELInformationTransformer;
 
@@ -109,6 +111,58 @@ public class MVELInformationTransformerTest {
         } catch (Exception e) {
         }
     }
+ 
+    @Test
+    public void testTransformDOM() {
+        org.w3c.dom.Document doc=null;
+        
+        try {
+            doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+            
+            org.w3c.dom.Element elem=doc.createElement("test");
+            doc.appendChild(elem);
+            
+            elem.appendChild(doc.createTextNode("This is a test"));
+            
+        } catch (Exception e) {
+            fail("Failed to build DOM: "+e);
+        }
+        
+        MVELInformationTransformer transformer=new MVELInformationTransformer();
+        
+        transformer.setExpression("import javax.xml.transform.TransformerFactory; "+
+                "import javax.xml.transform.Transformer; "+
+                "import javax.xml.transform.dom.DOMSource; "+
+                "import javax.xml.transform.stream.StreamResult; "+
+                "import java.io.ByteArrayOutputStream; "+
+                "ByteArrayOutputStream out = new ByteArrayOutputStream();"+
+                "DOMSource source = new DOMSource(information);"+
+                "StreamResult result = new StreamResult(out);"+
+                "TransformerFactory transFactory = TransformerFactory.newInstance();"+
+                "Transformer transformer = transFactory.newTransformer();"+
+                "transformer.transform(source, result);"+
+                "out.close();"+
+                "return new String(out.toByteArray());");
+        
+        try {
+            transformer.init();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Evaluator should have initialized: "+e);
+        }
+        
+        String value=transformer.transform(doc);
+        
+        if (value == null) {
+            fail("Null value returned");
+        }
+        
+        if (!value.contains("<test>This is a test</test>")) {
+            fail("Value does not contain original document information: "+value);
+        }
+    }
+
+
 
     public class TestInfo {
     	public String value="Hello";
